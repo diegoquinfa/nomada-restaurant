8d0be7a0bcf60e0365cc8a97a559a900d6d17299
Diego Andrés Quintana Fajardo <147205620+diegoquinfa@users.noreply.github.com>
2026-05-22 02:16:53 -0500

    this

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -42,6 +42,7 @@ export const Route = createRootRoute({
       { name: "twitter:image", content: `${SITE_URL}/images/open_graph.webp` },
     ],
     links: [
+      { rel: "preload", href: appCss, as: "style" },
       { rel: "stylesheet", href: appCss, fetchpriority: "high" },
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       { rel: "canonical", href: SITE_URL },
